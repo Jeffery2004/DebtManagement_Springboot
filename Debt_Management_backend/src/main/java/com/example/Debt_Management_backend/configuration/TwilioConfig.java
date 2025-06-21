b86e210e52f7974dc46c1576829ed405f4311cc1
Jeffery Abijah <jefferyabijah@gmail.com>
2025-06-21 10:22:07 +0530

    Update TwilioConfig.java

--- Debt_Management_backend/src/main/java/com/example/Debt_Management_backend/configuration/TwilioConfig.java
+++ Debt_Management_backend/src/main/java/com/example/Debt_Management_backend/configuration/TwilioConfig.java
@@ -1,7 +1,7 @@
 package com.example.Debt_Management_backend.configuration;
 
 public class TwilioConfig {
-    public static final String ACCOUNT_SID = "AC85a61e83cbf5c05edd4df5f4165919c0";
-    public static final String AUTH_TOKEN = "22ca5e4a026ef3a5d37884aca9b71b57";
-    public static final String VERIFY_SERVICE_SID = "VA1fe32f116e0edea0fae37a8212d78226";
+    public static final String ACCOUNT_SID = "SID";
+    public static final String AUTH_TOKEN = "TOKEN";
+    public static final String VERIFY_SERVICE_SID = "SERVICEID";
 }
